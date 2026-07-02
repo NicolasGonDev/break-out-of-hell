464ce7c939e613836e79a74d164afb2719179ebc
RubberEyes <nicolas.gon.dev@gmail.com>
2026-07-02 19:30:46 -0300

    release vs dev

--- src/plugins/setup.rs
+++ src/plugins/setup.rs
@@ -7,8 +7,11 @@ use crate::{
     },
 };
 
-use bevy::input::common_conditions::input_toggle_active;
 use bevy::prelude::*;
+
+#[cfg(feature = "inspector")]
+use bevy::input::common_conditions::input_toggle_active;
+#[cfg(feature = "inspector")]
 use bevy_inspector_egui::{bevy_egui::EguiPlugin, quick::WorldInspectorPlugin};
 
 pub struct SetupPlugin;
@@ -16,20 +19,26 @@ pub struct SetupPlugin;
 impl Plugin for SetupPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins((DefaultPlugins.set(plugins::window_plugin()),))
-            .add_plugins(EguiPlugin::default())
-            .add_plugins(
-                WorldInspectorPlugin::default().run_if(input_toggle_active(true, KeyCode::Enter)),
-            )
             .insert_resource(BrickMaterials::new())
             .add_systems(Startup, populate_brick_materials)
-            .add_systems(Startup, spawn_camera)
-            .add_systems(Startup, spawn_bricks.after(populate_brick_materials))
-            .add_systems(Startup, spawn_walls)
-            .add_systems(Startup, spawn_player)
-            .add_systems(Startup, spawn_ball)
+            .add_systems(
+                Startup,
+                (
+                    spawn_camera,
+                    spawn_bricks.after(populate_brick_materials),
+                    spawn_walls,
+                    spawn_player,
+                    spawn_ball,
+                ),
+            )
             .add_systems(Update, esc_exit)
             .add_systems(FixedUpdate, check_for_collisions)
             .add_systems(FixedUpdate, move_player)
             .add_systems(FixedUpdate, move_ball);
+
+        #[cfg(feature = "inspector")]
+        app.add_plugins(EguiPlugin::default()).add_plugins(
+            WorldInspectorPlugin::default().run_if(input_toggle_active(true, KeyCode::Enter)),
+        );
     }
 }
